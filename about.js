67582479bdad4113920399c5ef81c3fef9d2458d
NoelZeyn <127940099+NoelZeyn@users.noreply.github.com>
2023-05-29 10:24:35 +0700

    Add files via upload

---
+++ about.js
@@ -0,0 +1,9 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const customMenuToggle = document.querySelector(".custom-menu");
+//     const customMenu = document.querySelector(".Navbar");
+
+//     customMenuToggle.addEventListener("click", function () {
+//       customMenu.classList.toggle("show");
+//     });
+//   });
+
