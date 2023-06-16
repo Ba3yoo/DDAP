643636421230cb64e2c9a1147aab05f7c7d253a3
IStoum <ahmadakrom563@gmail.com>
2023-06-16 23:43:08 +0700

    hover

--- about/about.js
+++
@@ -1,9 +0,0 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const customMenuToggle = document.querySelector(".custom-menu");
-//     const customMenu = document.querySelector(".Navbar");
-
-//     customMenuToggle.addEventListener("click", function () {
-//       customMenu.classList.toggle("show");
-//     });
-//   });
-
