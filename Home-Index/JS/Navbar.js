be1e888e20b6b236c2f40650f5f9fd47947b8eea
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-06-17 10:07:12 +0700

    Edited Home-Index

--- Home-Index/JS/Navbar.js
+++ Home-Index/JS/Navbar.js
@@ -1,16 +1,7 @@
-const navbar = document.querySelector('nav')
-const menu = navbar.querySelector('ul')
-const openBtn = navbar.querySelector('.fa-bars')
-const closeBtn = menu.querySelector('.fa-times')
+const navbar = document.querySelector("nav");
+const menu = navbar.querySelector("ul");
+const openBtn = navbar.querySelector(".fa-bars");
+const closeBtn = navbar.querySelector(".fa-times");
 
-openBtn.onclick = () => menu.classList.toggle('menu')
-closeBtn.onclick = () => menu.classList.toggle('menu')
-
-// // Mendapatkan elemen-elemen yang dibutuhkan
-// const menuToggle = document.querySelector('.fa-solid');
-// const navMenu = document.querySelector('nav ul');
-
-// // Menambahkan event listener ke menu toggle
-// menuToggle.addEventListener('click', () => {
-//   navMenu.classList.toggle('menu'); // Menambahkan atau menghapus class 'menu'
-// });
+openBtn.onclick = () => menu.classList.toggle("menu-hidden");
+closeBtn.onclick = () => menu.classList.toggle("menu-hidden");
