3befaf6e0b2aaa09d9a6821534995d87bee44aae
Ikram-sabila <ikramsabila@gmail.com>
2023-06-18 09:46:40 +0700

    new

--- Article/Gambar/style.js
+++ Article/Gambar/style.js
@@ -1,4 +1,7 @@
-function toggleNavbar() {
-  var navbarItems = document.getElementById("navbarItems");
-  navbarItems.classList.toggle("active");
-}
+const navbar = document.querySelector("nav");
+const menu = navbar.querySelector("ul");
+const openBtn = navbar.querySelector(".fa-bars");
+const closeBtn = navbar.querySelector(".fa-times");
+
+openBtn.onclick = () => menu.classList.toggle("menu-hidden");
+closeBtn.onclick = () => menu.classList.toggle("menu-hidden");
