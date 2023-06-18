709f34406e2adf161e6b1fc4ac4e67640145e8ce
IStoum <ahmadakrom563@gmail.com>
2023-06-18 10:07:14 +0700

    pppp

--- about/about_.js
+++ about/about_.js
@@ -2,11 +2,5 @@ const mobileMenuIcon = document.querySelector('.mobile_menu_icon');
 const mobileMenuList = document.querySelector('.mobile_menu_list');
 
 mobileMenuIcon.addEventListener('click', function() {
-    // mobileMenuIcon.classlist.toggle('show');
     mobileMenuList.classList.toggle('show');
 });
-
-// mobileMenuIcon.addEventListener('click', function() {
-//   mobileMenuIcon.classlist.toggle('show');
-//   // mobileMenuList.classList.toggle('show');
-// });
